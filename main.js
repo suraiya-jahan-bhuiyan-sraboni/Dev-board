9c395005c16b540fcea71224e401c1b8dbcc7f20
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-03-08 00:06:23 +0600

    multiple color on button click impelementation

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 document.getElementById("discover_new").addEventListener("click", function () {
     window.location.href="blog.html";
 });
+
+document.getElementById("clear_history").addEventListener("click", function () {
+    document.getElementById("history").innerHTML = "";
+})
+function randomColors() {
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += Math.floor((Math.random() * 16)).toString(16);
+    }
+    return color;
+
+}
+document.getElementById("color_change").addEventListener("click", function () {
+    document.getElementById("body").style.backgroundColor=randomColors();
+
+})
